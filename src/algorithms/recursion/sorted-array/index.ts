1848f8ee54a6142410c8a44faa0c5631beee3881
xanity-07 <th3loot69@gmail.com>
2025-05-26 15:52:51 -0400

    added iterative solution to is sorted array

--- src/algorithms/recursion/sorted-array/index.ts
+++ src/algorithms/recursion/sorted-array/index.ts
@@ -1,3 +1,15 @@
+//* Iterative Solution
+function isSortedArr(sorted: number[], idx: number = 0) {
+  for (idx; idx < sorted.length; idx++) {
+    if (sorted[idx] > sorted[idx + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isSortedArr([1, 2, 3, 4, 5, 7, 8, 9], 0));
+
+//* Recursive Solution
 function isSorted(sorted: number[], index: number = 0): boolean {
   if (index === sorted.length - 1) return true;
   return sorted[index] < sorted[index + 1] && isSorted(sorted, index + 1);
